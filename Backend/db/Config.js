c4fc89ea9cb50bc313ce2fdbb06aaf3aae25be41
Noman Hassan <137282216+NomanHassan4518@users.noreply.github.com>
2024-02-29 23:51:00 +0500

    Update Config.js

--- Backend/db/Config.js
+++ Backend/db/Config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+        await mongoose.connect("mongodb+srv://malikhassanhu55:2W64GIuF7ulvvuo8@cluster0.xhhzsjf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Connect to MongoDB successfully")
     } catch (error) {
         console.log("Connect failed " + error.message )
